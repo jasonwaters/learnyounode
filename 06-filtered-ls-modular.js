eb88f5f8113aa3c99a70209436f9de33f9f784ec
Jason Waters <waters@workfront.com>
2016-01-11 13:43:40 -0700

    file upercaserer

--- 06-filtered-ls-modular.js
+++ 06-filtered-ls-modular.js
@@ -1,4 +1,4 @@
-var dirlistmodule = require('./dirlistmodule.js');
+var dirlistmodule = require('./06-filtered-ls-module.js');
 
 dirlistmodule(process.argv[2], process.argv[3], function(err, data) {
 	if(!err) {
--- 12-http-uppercaserer.js
+++ 12-http-uppercaserer.js
@@ -4,7 +4,13 @@ var map = require('through2-map');
 var port = parseInt(process.argv[2]);
 
 var server = http.createServer(function(request, response) {
+    if(request.method !== 'POST') {
+        return request.end('only POST is supported');
+    }
 
+    request.pipe(map(function(chunk) {
+        return chunk.toString().toUpperCase();
+    })).pipe(response);
 });
 
 server.listen(port);
